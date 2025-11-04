25a2c9dff14a9cabd8077fec094a2c8300c4026a
TC Fraser <tcfraser237@gmail.com>
2025-11-04 16:52:44 +0100

    first draft of inventiones submission

--- submissions/build_arxiv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp ../figure-sixj-upper-bound.pdf              arxiv/figure-sixj-upper-bound.pdf
-cp ../figure-triangle-cayley-menger.pdf        arxiv/figure-triangle-cayley-menger.pdf
-cp ../figure-triangle-tetrahedron-matrices.pdf arxiv/figure-triangle-tetrahedron-matrices.pdf
-cp ../main.tex                                 arxiv/main.tex
-cp ../main.bbl                                 arxiv/main.bbl
-cd arxiv
-tar -cvvf arxiv.tar *
-cd ..
